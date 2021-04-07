2b322f5de01819e71791f3365228b0d6c2cb2cc7
Cedric0303 <chenjunli7@gmail.com>
2021-04-07 12:13:57 +0800

    fix food order POST

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -43,7 +43,7 @@ const addFoodToOrder = async (req, res) => {
     const food = await db.db.collection('food').findOne({name: req.params.name})
     const customer = await db.db.collection('customer').findOne({loginID: req.body.loginID})
     await db.db.collection('order').insertOne({
-        item: [{foodID: food.foodID, name: food.name, count: req.body.count}], 
+        item: [{foodID: food._ID, name: food.name, count: req.body.count}], 
         timestamp: new Date(),
         vendorID: req.body.vendorID, 
         customerID: customer.loginID, 
