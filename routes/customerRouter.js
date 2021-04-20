ddd7d69a1dbb605e4bf66d7beadb657e770ab90c
Cedric0303 <chenjunli7@gmail.com>
2021-04-20 16:42:31 +0800

    add router missing comments

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -6,12 +6,16 @@ const customerRouter = express.Router()
 // add the customer controller
 const customerController = require('../controllers/customerController.js')
 
+// return default customer homescreen
 customerRouter.get('/', customerController.getCustomerHome)
 
+// get food items from the database and return it
 customerRouter.get('/menu', customerController.getMenu)
 
+// return information for a given food item
 customerRouter.get('/menu/:name', customerController.getFoodDetails)
 
+// create a new order and add a specified food into the order
 customerRouter.post('/menu/:name', customerController.addFoodToOrder)
 
 module.exports = customerRouter
--- routes/vendorRouter.js
+++ routes/vendorRouter.js
@@ -6,14 +6,21 @@ const vendorRouter = express.Router()
 // add the vendor controller
 const vendorController = require('../controllers/vendorController.js')
 
+// return default vendor home screen
 vendorRouter.get('/', vendorController.getVendorHome)
+
+// set van status using location provided
 
 vendorRouter.post('/:id', vendorController.postVendor)
 
+// return a specific vendor van
+
 vendorRouter.get('/:id', vendorController.getVendor)
 
+// return orders of a specific vendor van
 vendorRouter.get('/:id/orders', vendorController.getOrders)
 
+// sets a specific order as fulfilled
 vendorRouter.post('/:id/orders/:orderID', vendorController.fulfilledOrder)
 
 module.exports = vendorRouter
