fe174d230711e0b06f57c30cde7746dc2ab5afa6
Cedric0303 <chenjunli7@gmail.com>
2021-04-26 15:56:37 +0800

    change use of public to html for style css

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 8080
 
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('html'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
