b7f3cdcd57edd6b5b924ec1edea145edf548146c
jeongwoo seo <79891380+jwoo2023@users.noreply.github.com>
2021-05-02 00:24:31 +1000

    updated cart api for ordering on menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -38,6 +38,124 @@ function filterMenu() {
     }
 }
 
+// shopping cart api--------------------------
+var shoppingCart = (function () {
+    // ---------------------
+    // private methods
+    // ---------------------
+    // init empty cart
+    cart = [];
+    
+    // item object constructor
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    // save cart in local storage
+    function saveCart() {
+        localStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+    // load cart into 'cart' array
+    function loadCart() {
+        cart = JSON.parse(localStorage.getItem('shoppingCart'));
+    }
+    // if cart in localstorage is not empty, load the cart automatically
+    if (sessionStorage.getItem('shoppingCart') != null) {
+        loadCart();
+    }
+
+    // ---------------------
+    // public methods
+    // ---------------------
+    var obj = {};
+
+    // add to cart
+    obj.addItemToCart = function(name, price, count) {
+        // if item exists in cart, increment by 1
+        for(var item in cart) {
+            if(cart[item].name === name) {
+            cart[item].count ++;
+            saveCart();
+            return;
+            }
+        }
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    }
+
+    // update count for item
+    obj.setCountForItem = function(name, count) {
+        for(var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // remove item from cart
+    obj.removeItemFromCart = function(name) {
+        // if item exists in cart, decrement by 1
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart[item].count --;
+                if(cart[item].count === 0) {
+                    cart.splice(item, 1);
+                }
+            }
+        }
+        saveCart();
+    }
+
+    // remove all items from cart
+    obj.removeItemFromCartAll = function(name) {
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart.splice(item, 1);
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // return total quantity of items in cart
+    obj.totalCount = function() {
+        var totalCount = 0;
+        for(var item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount;
+    }
+
+    // return total cost of items in cart
+    obj.totalCart = function() {
+        var totalCart = 0;
+        for(var item in cart) {
+            totalCart += cart[item].price * cart[item].count;
+        }
+        return Number(totalCart.toFixed(2));
+    }
+
+    // get cart, returns a copy of the current cart
+    obj.getCart = function() {
+        var cartCopy = [];
+        for(i in cart) {
+            item = cart[i];
+            itemCopy = {};
+            for(property in item) {
+                itemCopy[property] = item[property];
+            }
+            itemCopy.total = Number(item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy);
+        }
+        return cartCopy;
+    }
+})
+
 // expand/collapse cart
 function toggleCart() {
     cartcontainer = document.getElementById('cartcontainer');
@@ -48,7 +166,7 @@ function toggleCart() {
         // hide cart contents
         document.getElementById('cart').style.display = 'none';
         // change view cart button to 'view cart' 
-        document.getElementById('vcartbutton').innerHTML = 'View Cart';
+        document.getElementById('vcartbutton').innerHTML = 'Cart&nbsp;(<span class="cart-count"></span>)';
     } else {
         // expand cart
         cartcontainer.style.height = '30vh';
@@ -59,15 +177,14 @@ function toggleCart() {
     }
 }
 
-// shopping cart --------------------------
-
-
-// register events to elements
+// ---------register events to elements----------
+// menu category filter buttons
 categoryCheckboxes = document.getElementsByName('catcheck');
 for (var i=0; i < categoryCheckboxes.length; i++) {
     categoryCheckboxes[i].addEventListener("click", filterMenu, false);
 }
 
+// view cart button
 document.getElementById('vcartbutton').addEventListener("click", toggleCart, false)
 
 // run the filter on load
