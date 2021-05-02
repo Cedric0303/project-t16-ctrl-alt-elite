8fb42e96d6f9d014b1f3f379b9f737c2328a782f
Cedric0303 <chenjunli7@gmail.com>
2021-05-02 17:12:27 +0800

    change to show only 5 nearest vans

--- public/js/map.js
+++ public/js/map.js
@@ -1,33 +1,117 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoianVubGljIiwiYSI6ImNrbzVpY3psNjAzODMydnBiYWVycXd2cDIifQ.sEdF7QZVuor4EbMa3hWdfA'
+// harvesine distance between 2 location
+// code modified and obtained from https://stackoverflow.com/a/48805273
 
-var map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [144.95878, -37.7983416], // starting position [lng, lat]
-    zoom: 14 // starting zoom
-});
+function distance([long1, lat1], [long2, lat2]) {
+    const toRad = angle => (Math.PI / 180) * angle;
+    const dist = (a, b) => (Math.PI / 180) * (a - b);
+    const RADIUS_OF_EARTH_IN_KM = 6371;
 
-var geolocate = new mapboxgl.GeolocateControl({
+    const dLat = dist(lat2, lat1);
+    const dLong = dist(long2, long1);
+    lat1 = toRad(lat1);
+    lat2 = toRad(lat2);
+
+    // harvesine formula
+    const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.pow(Math.sin(dLong / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+    const c = 2 * Math.asin(Math.sqrt(a));
+
+    var finalDistance = RADIUS_OF_EARTH_IN_KM * c;
+    return finalDistance;
+}
+
+function createMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoianVubGljIiwiYSI6ImNrbzVpY3psNjAzODMydnBiYWVycXd2cDIifQ.sEdF7QZVuor4EbMa3hWdfA'
+    // generates map
+    var map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [144.95878, -37.7983416], // starting position [lng, lat]
+        zoom: 14 // starting zoom
+    });
+    // location getter button
+    var geolocate = new mapboxgl.GeolocateControl({
     positionOptions: {
-        enableHighAccuracy: false
+        enableHighAccuracy: true
       },
       trackUserLocation: true,
       showAccuracyCircle: false
     });
-
-map.addControl(geolocate)
-
-var vans_array = vans
-for (var van in vans_array) {
-    var latitude = vans_array[van].latitude
-    var longitude = vans_array[van].longitude
-    var van_marker = new mapboxgl.Marker({
-        color: '#FFFFFF'
-        }) // initialize a new marker
-        .setLngLat([longitude, latitude]) // Marker [lng, lat] coordinates
-        .addTo(map); // Add the marker to the map
+    return {map, geolocate};
 }
 
+function createLocationMarker() {
+    return new mapboxgl.Marker({color: '#000000'}) 
+            .setLngLat(map.getCenter()) // Marker [lng, lat] coordinates
+            .addTo(map)
+}
+
+function createVanMarker(points) {
+    var vanMarkers = []
+    for (i in points) {
+        if (i==5) {
+            break;
+        }
+        var long = points[i][0].longitude
+        var lat = points[i][0].latitude
+        vanMarkers.push([
+            points[i][0],
+            new mapboxgl.Marker({color: '#65737D'}) 
+            .setLngLat([long, lat]) // Marker [lng, lat] coordinates
+            .addTo(map)]); // Add the marker to the map
+    }
+    return vanMarkers
+}
+
+function createMarkers(curPosition, vans) {
+    var position = curPosition.getLngLat();
+    var point_distance = []
+    for (var van in vans) {
+        var latitude = vans[van].latitude
+        // console.log(latitude)
+        var longitude = vans[van].longitude
+        // console.log(longitude)
+        var dist = distance([longitude, latitude],[position.lng, position.lat]);
+        point_distance.push([vans[van], dist])
+    }
+    point_distance.sort((a, b) => (a[1] > b[1] ? 1: -1));
+    var vanMarkers = createVanMarker(point_distance);
+    return vanMarkers;
+}
+
+var {map, geolocate} = createMap()
+map.addControl(geolocate)
+geolocate.on('geolocate', function() {
+    geolocate.trigger();
+    setTimeout(function () {
+        curPosition.remove();
+        curPosition = createLocationMarker()}, 5000);
+        point_distance = createVanArray(map, curPosition, vans);
+        point_distance.sort((a, b) => (a[1] > b[1] ? 1: -1));
+        vanMarkers = createVanMarker(point_distance)
+        // curPosition = map.getCenter()
+    });
+var curPosition = createLocationMarker()
+var vanMarkers = createMarkers(curPosition, vans)
+
+// map.on('load', function() {
+//     geolocate.trigger(); //<- Automatically activates geolocation
+
+// });
+// if (!geolocated) {
+//     var marker = new mapboxgl.Marker({
+//         color: '#000000'
+//         }) // initialize a new marker
+//         .setLngLat([144.95878, -37.7983416]) // Marker [lng, lat] coordinates
+//         .addTo(map); // Add the marker to the map
+// }
+// else {
+//     var marker = new mapboxgl.Marker({
+//         color: '#000000'
+//         }) // initialize a new marker
+//         .setLngLat([cur_position.coords.longitude, cur_position.coords.latitude]) // Marker [lng, lat] coordinates
+//         .addTo(map); // Add the marker to the map
 // var map = new mapboxgl.Map({
 //     container: 'map',
 //     style: "https://api.jawg.io/styles/jawg-streets.json?access-token=WNOUDThn1YkriWZNGrnIqTGPSf40C2FkPsYJ1anAcEAcjBHopzZfVO0OddI9bRLI",
@@ -47,27 +131,3 @@ for (var van in vans_array) {
   
 //   // Add the geocoder to the map
 //   map.addControl(geocoder);
-
-var marker = new mapboxgl.Marker({
-    color: '#000000'
-    }) // initialize a new marker
-    .setLngLat([144.95878, -37.7983416]) // Marker [lng, lat] coordinates
-    .addTo(map); // Add the marker to the map
-
-// function getNearestVans(vans) {
-//     // var cur_loc = geolocate.trigger()
-//     // var cur_loc = [144.95878, -37.7983416]
-//     for (van in vans) {
-//         console.log(van_loc)
-//         new mapboxgl.Marker({
-//             color: '#FFFFFF'
-//             }) // initialize a new marker
-//             .setLngLat([van.longitude, van.latitude]) // Marker [lng, lat] coordinates
-//             .addTo(map); // Add the marker to the map
-//     }
-// }
-
-// map.on('load', function() {
-//     geolocate.trigger(); //<- Automatically activates geolocation
-//   });
-// getNearestVans(vans);
