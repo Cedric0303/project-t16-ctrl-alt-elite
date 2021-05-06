5a23fd5c378846b1f3ee87061a0fb63f7e52b443
Cedric0303 <chenjunli7@gmail.com>
2021-05-06 12:10:59 +0800

    move secret ke to environment variables

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
-	secret: 'secret',
+	secret:	process.env.SECRET_KEY,
 	resave: true,
 	saveUninitialized: true
 }));
