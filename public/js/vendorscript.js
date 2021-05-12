3c801f28fd10837ea9b8f403a281e920f230e2a9
Cedric0303 <chenjunli7@gmail.com>
2021-05-12 15:49:49 +0800

    use prev van address if no address input

--- public/js/vendorscript.js
+++ public/js/vendorscript.js
@@ -1,5 +1,10 @@
 function submitForm() {
-    document.form.address.value = document.getElementById("vantextdescription").value
+    if (document.getElementById("vantextdescription").value == 0) {
+        document.form.address.value =  document.getElementById("vantextdescription").placeholder
+    }
+    else {
+        document.form.address.value = document.getElementById("vantextdescription").value
+    }
     document.form.longitude.value = curPos.long
     document.form.latitude.value = curPos.lat
     document.forms["form"].submit();
