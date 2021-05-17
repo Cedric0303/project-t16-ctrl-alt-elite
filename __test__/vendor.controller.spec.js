56daa0cf0d3bbb4cb4a8f1f6783385e0e1ffa7d6
Cedric0303 <chenjunli7@gmail.com>
2021-05-17 14:44:10 +0800

    add more integration test

--- __test__/vendor.controller.spec.js
+++ __test__/vendor.controller.spec.js
@@ -1,21 +1,95 @@
 const mongoose = require('../controllers/databaseController.js')
 const db = mongoose.connection
+// const jest = require('jest')
 
 const vendorController = require('../controllers/vendorController')
+const vendorToken = require('../controllers/vendorToken')
 
 const Vendor = require('../models/vendorSchema')
 
-describe("Unit testing getVendorHome from vendorController.js", () => {
+describe('#1 Unit testing getVendorHome', () => {
     const req = {}
     const res = {
-        render: jest.fn(),
+        render: jest.fn()
     }
     beforeAll(() => {
         res.render.mockClear()
-        vendorController.getVendorHome(req, res)
     })
-    test("Test getVendorHome()", () => {
-            expect(res.render).toHaveBeenCalledTimes(1);
-            expect(res.render).toHaveBeenCalledWith('vendor/login', {layout: 'vendor/main'})
+    test("Test getVendorHome()", async () => {
+        vendorController.getVendorHome(req, res)
+        expect(res.render).toHaveBeenCalledTimes(1)
+        expect(res.render).toHaveBeenCalledWith('vendor/login', {layout: 'vendor/main'})
+    })
+})
+
+describe("#2 Unit testing postVendor (logged in)", () => {
+    const req = {
+        params: {
+            vanID: 'Diner_Driver'
+        },
+        body: {
+            address: '169 Rathdowne St',
+            latitude: -37.802852,
+            longitude: 144.969487
+        }
+    }
+    const res = {
+        render: jest.fn(),
+        redirect: jest.fn(),
+        status: jest.fn().mockReturnValue(400),
+    }
+    beforeAll (() => {
+        res.render.mockClear()
+        res.redirect.mockClear()
+        res.status.mockClear()
+        vendorToken.loggedIn = jest.fn(x => true)
+        Vendor.updateOne = jest.fn().mockResolvedValue({
+            "acknowledged" : true, 
+            "matchedCount" : 1, 
+            "modifiedCount" : 1
         })
+    })
+    test("Test postVendor() (logged in)", async () => {
+        await vendorController.postVendor(req, res)
+        expect(res.render).toHaveBeenCalledTimes(0);
+        expect(res.redirect).toHaveBeenCalledTimes(1);
+        expect(res.redirect).toHaveBeenCalledWith('/vendor/' + req.params.vanID + '/orders')
+    })
+})
+
+describe("#3 Unit testing postVendor (not logged in)", () => {
+    const req = {
+        params: {
+            vanID: 'Diner_Driver'
+        },
+        body: {
+            address: '169 Rathdowne St',
+            latitude: -37.802852,
+            longitude: 144.969487
+        }
+    }
+    const res = {
+        render: jest.fn(),
+        redirect: jest.fn(),
+        status: jest.fn()
+    }
+    beforeAll(() => {
+        res.render.mockClear()
+        res.redirect.mockClear()
+        res.status.mockClear()
+        vendorToken.loggedIn = jest.fn(x => false)
+        Vendor.updateOne = jest.fn().mockResolvedValue({
+            "acknowledged" : true, 
+            "matchedCount" : 1, 
+            "modifiedCount" : 1
+        })
+    })
+
+    test("Test postVendor() (not logged in)", async () => {
+        await vendorController.postVendor(req, res)
+        expect(res.redirect).toHaveBeenCalledTimes(0);
+        expect(res.render).toHaveBeenCalledTimes(1);
+        expect(res.status).toHaveBeenCalledWith(402)
+        expect(res.render).toHaveBeenCalledWith('vendor/notloggedin', {layout: 'vendor/main'})
+    })
 })
--- __test__/vendor.integration.spec.js
+++ __test__/vendor.integration.spec.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 const app = require('../app'); // the express server
 
-describe('Integration test', () => {
+describe('#1 Integration test (logged in)', () => {
 
     let agent = request.agent(app);
     let cookie = null;
@@ -107,6 +107,84 @@ describe('Integration test', () => {
 				expect(response.statusCode).toBe(302)
 				expect(response.type).toBe('text/plain')
                 expect(response.text).toBe('Found. Redirecting to /vendor')
+        });
+    });
+});
+
+describe('#2 Integration test (not logged in)', () => {
+
+    let agent = request.agent(app);
+
+    test('Test 1: GET vendor home/login page', async () => {
+		return agent
+			.get('/vendor')
+			.then((response) => {
+                expect(response.statusCode).toBe(200)
+                expect(response.type).toBe('text/html')
+                assert(response.text.includes('Snacks in a Van'))
+			});
+    });
+  
+    test('Test 2: GET vendor open van', async () => {
+		return agent
+			.get('/vendor/Diner_Driver')
+			.then((response) => {
+                expect(response.statusCode).toBe(402)
+                expect(response.type).toBe('text/html')
+                assert(response.text.includes('Not logged in!'))
+			});
+    });
+
+    test('Test 3: GET vendor close van', async () => {
+        return agent
+			.get('/vendor/Diner_Driver/close')
+			.then((response) => {
+				expect(response.statusCode).toBe(402)
+                expect(response.type).toBe('text/html')
+                assert(response.text.includes('Not logged in!'))
+        });
+    });
+
+    test('Test 4: POST vendor open van', async () => {
+      return agent
+          .post('/vendor/Diner_Driver/open')
+          .send({address: '169 Rathdowne St',
+                  latitude: -37.802852,
+                  longitude: 144.969487})
+          .then((response) => {
+            expect(response.statusCode).toBe(402)
+            expect(response.type).toBe('text/html')
+            assert(response.text.includes('Not logged in!'))
+          });
+    });
+
+    test('Test 5: GET vendor orders', async () => {
+        return agent
+			.get('/vendor/Diner_Driver/orders')
+			.then((response) => {
+				expect(response.statusCode).toBe(402)
+                expect(response.type).toBe('text/html')
+                assert(response.text.includes('Not logged in!'))
+			});
+    });
+
+    test('Test 6: GET vendor past orders', async () => {
+        return agent
+			.get('/vendor/Diner_Driver/pastorders')
+			.then((response) => {
+				expect(response.statusCode).toBe(402)
+                expect(response.type).toBe('text/html')
+                assert(response.text.includes('Not logged in!'))
+        });
+    });
+    
+    test('Test 7: GET vendor logout', async () => {
+        return agent
+			.get('/vendor/Diner_Driver/logout')
+			.then((response) => {
+				expect(response.statusCode).toBe(402)
+                expect(response.type).toBe('text/html')
+                assert(response.text.includes('Not logged in!'))
         });
     });
 });
