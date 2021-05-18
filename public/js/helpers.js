06593d36d3d09a1413276cebf8c9b121a8c1a1cf
Allamand4 <80309830+Allamand4@users.noreply.github.com>
2021-05-18 19:22:48 +1000

    pastOrders formatting and new past orders helper for date/time

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -25,6 +25,24 @@ var register = function(Handlebars) {
             formatted = date+" at "+time;
             return formatted;
         },
+
+        formatDatePrevOrders: function (datetime) {
+            var dt = new Date(datetime);
+            formatted = "";
+            ampm = "";
+            month = dt.toLocaleString('en-AU', {month: 'short' });
+            day = dt.getDate();
+            date = dt.toDateString();
+            time = dt.toLocaleTimeString("default", {
+                timeZone: "Australia/Melbourne",
+                hour: '2-digit', 
+                minute:'2-digit',
+            
+            });
+
+            formatted = time.toUpperCase() + " " + month + " " + day;
+            return formatted;
+        },
         // https://stackoverflow.com/questions/10232574/handlebars-js-parse-object-instead-of-object-object
         json: function (context) {
             return JSON.stringify(context);
