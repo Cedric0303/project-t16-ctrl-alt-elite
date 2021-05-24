915d418f3d800f2cb3018cc62a49ed23c7325609
Cedric0303 <chenjunli7@gmail.com>
2021-05-24 10:50:45 +0800

    fix completed live order status

--- public/js/liveorder.js
+++ public/js/liveorder.js
@@ -1,18 +1,47 @@
-var totalTime = 0
-function timeElapsed(orderinfo,  totalTime) {
-    if (!totalTime) {
+var totalSeconds = 0
+function timeElapsed(orderinfo,  totalSeconds) {
+    time = totalSeconds
+    if (!totalSeconds) {
         orderTime = new Date(orderinfo.timestamp)
-        totalTime = Math.round((new Date().getTime() -  orderTime.getTime()) / 1000)
+        totalSeconds = Math.round((new Date().getTime() -  orderTime.getTime()) / 1000)
     }
-    else totalTime += 1
-    minute = Math.floor(totalTime / 60)
-    seconds = totalTime % 60
-    document.getElementById('timeElapsed').innerHTML = 'Time elapsed: ' + minute + 'm, ' + seconds + 'sec'
+    else totalSeconds += 1
+    hour = Math.floor(totalSeconds / 3600)
+    time = totalSeconds % 3600
+    minute = Math.floor(time / 60)
+    second = time % 60
+    setTimeElapsed(hour, minute, second)
 }
-setInterval(()=> {
-    timeElapsed(orderinfo, totalTime)
-}, 1000)
 
+function completeTime(orderinfo, totalSeconds) {
+    time = totalSeconds
+    orderTime = new Date(orderinfo.timestamp)
+    completedTime = new Date(orderinfo.completedTimestamp)
+    totalSeconds = Math.round((completedTime.getTime() -  orderTime.getTime()) / 1000)
+    hour = Math.floor(totalSeconds / 3600)
+    time = totalSeconds % 3600
+    minute = Math.floor(time / 60)
+    second = time % 60
+    setTimeElapsed(hour, minute, second)
+}
+
+function setTimeElapsed(hour, minute, second) {
+    if (!hour) {
+        document.getElementById('timeElapsed').innerHTML = 'Time elapsed: ' + minute + 'm, ' + second + 'sec'
+    }
+    else {
+        document.getElementById('timeElapsed').innerHTML = 'Time elapsed: '+ hour + 'h, ' + minute + 'm, ' + second + 'sec'
+    }
+}
+
+if (document.getElementById('orderStatusText').innerHTML != 'Completed') {
+    setInterval(()=> {
+        timeElapsed(orderinfo, totalSeconds)
+    }, 1000)
+}
+else {
+    completeTime(orderinfo, totalSeconds)
+}
 
 const socket = io()
 socket.emit('orderID', orderinfo.orderID);
