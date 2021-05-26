636f1de71c82fa3165324b19ab86040a41dc99ca
Cedric0303 <chenjunli7@gmail.com>
2021-05-26 21:44:30 +0800

    add constants module

---
+++ controllers/constants.js
@@ -0,0 +1,13 @@
+const MODIFYTIME = 10 * 60                      // 10 minutes
+const DISCOUNTTIME = 15 * 60                    // 15 minutes
+const DISCOUNTVALUE = 20 / 100                  // 20%
+const CUSTOMERTOKENTIME = 2 * 60 * 60 * 1000    // 2 hours
+const VENDORTOKENTIME = 12 * 60 * 60 * 1000     // 12 hours
+
+module.exports = {
+    MODIFYTIME,
+    DISCOUNTTIME,
+    DISCOUNTVALUE,
+    CUSTOMERTOKENTIME,
+    VENDORTOKENTIME
+}
